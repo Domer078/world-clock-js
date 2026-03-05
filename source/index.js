ebb4f5d7d36208e243b347100a663e309fcdfb52
Domer078 <khomotsomabala05@gmail.com>
2026-03-05 16:15:07 +0200

    Updated code to improve readability

--- source/index.js
+++ source/index.js
@@ -28,21 +28,20 @@ function updateCity(event) {
   if (cityTimeZone === "current") {
     cityTimeZone = moment.tz.guess();
   }
-
   let cityName = cityTimeZone.replace("_", "").split("/")[1];
   let cityTime = moment().tz(cityTimeZone);
   let citiesElement = document.querySelector("#cities");
-  citiesElement.innerHTML = `
-      <div class="city" id="Monaco">
+  citiesElement.innerHTML = `  
+        <div class="city">
         <div>
           <h2>${cityName}</h2>
           <div class="date">${cityTime.format("MMMM Do YYYY")}</div>
         </div>
-     <div class="time">${cityTime.format(
-       "h:mm:ss",
-     )} <small>${cityTime.format("A")}</small></div>
-        
-      </div>`;
+        <div class="time">${cityTime.format(
+          "h:mm:ss",
+        )} <small>${cityTime.format("A")}</small></div>
+      </div>
+       <a href="/">All cities</a>`;
 }
 
 updateTime();
