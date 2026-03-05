5cbd4c9a1e313e93230350cf7187586e08a05409
Domer078 <khomotsomabala05@gmail.com>
2026-03-05 14:33:59 +0200

    Refactor code and remove unnecessary lines

--- source/index.js
+++ source/index.js
@@ -1,8 +1,8 @@
 function updateTime() {
   let londonElement = document.querySelector("#London");
   if (londonElement) {
-    let londonDateElement = document.querySelector("#date");
-    let londonTimeElement = document.querySelector("#time");
+    let londonDateElement = document.querySelector(".date");
+    let londonTimeElement = document.querySelector(".time");
     let londonTime = moment().tz("Europe/London");
 
     londonDateElement.innerHTML = londonTime.format("MMMM Do YYYY");
@@ -11,14 +11,14 @@ function updateTime() {
     );
   }
 
-  let torontoElement = document.querySelector("#Toronto");
-  if (torontoElement) {
-    let torontoDateElement = document.querySelector("#date");
-    let torontoTimeElement = document.querySelector("#time");
-    let torontoTime = moment().tz("America/Toronto");
+  let melbourneElement = document.querySelector("#Melbourne");
+  if (melbourneElement) {
+    let melbourneDateElement = document.querySelector(".date");
+    let melbourneTimeElement = document.querySelector(".time");
+    let melbourneTime = moment().tz("Australia/Melbourne");
 
-    torontoDateElement.innerHTML = torontoTime.format("MMMM Do YYYY");
-    torontoTimeElement.innerHTML = torontoTime.format(
+    melbourneDateElement.innerHTML = melbourneTime.format("MMMM Do YYYY");
+    melbourneTimeElement.innerHTML = melbourneTime.format(
       "h:mm:ss [<strong>]A[</strong>]",
     );
   }
